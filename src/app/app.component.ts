3f5e4adea6e3823e7df24f8ad7a8b9e4ff963378
JuanCarlos <juan.velez@iroute.com.ec>
2022-05-07 15:59:36 -0500

    avances en proiyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontEnd';
+  //title = 'FrontEnd Cliente';
+  /* Interpolacion y PropertyBinding
+  textoPlaceholder='Escriba aqui por favor';
+  nombre= 'Princeso';
+  deshabilitado=true;
+  imgSrc="https://www.kindpng.com/picc/m/75-750111_angularjs-hd-png-download.png";
+  constructor(){
+    setInterval(()=>this.deshabilitado=false,3000);
+  }
+  getSuma(n1: number,n2: number){
+    return n1 + n2;
+  };*/
+  /* Event Binding
+  texto= 'Esto es un video sobre event binding';
+  cambiarTexto():void{
+    this.texto='En el PROXIMO video veremos Two Way data-binding';
+  }*/
+  //texto= 'Hola a todos';
+
+  listaEstudiantes: any[]= [
+    { nombre:"Juank Velez", estado:"Promocionado" },
+    { nombre:"Patricia Funes", estado:"Libre" },
+    { nombre:"Juan Garcia", estado:"Regular" },
+    { nombre:"Martha Perez", estado:"Regular" },
+    { nombre:"Nicolas Gomez", estado:"Libre" }
+  ]
+
+  mostrar=true;
+  toogle(): void{
+    this.mostrar= !this.mostrar;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { LoginComponent } from './components/inicio/login/login.component';
+import { RegisterComponent } from './components/inicio/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
