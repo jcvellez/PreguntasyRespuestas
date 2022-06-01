afbe8364b090edf2ab424b273c87843d7d5fe841
jcvellez <jcvellez@live.com>
2022-06-01 07:11:48 -0500

    Clase 28 voy el minuto 4:12. Revisar los direccionamientos que no esta funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 import { LoginComponent } from './components/inicio/login/login.component';
 import { RegisterComponent } from './components/inicio/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/bienvenidos', pathMatch: 'full' },
-  { path: 'bienvenidos', component: BienvenidaComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+  { path: 'inicio', component: InicioComponent, children: [
+      { path: '', component: BienvenidaComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent },
+    ]
+  },
   { path: '**', redirectTo: '/bienvenidos', pathMatch: 'full' }
 ];
 
