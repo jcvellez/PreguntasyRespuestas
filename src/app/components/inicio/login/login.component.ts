4a537d445b293a43c97fafcf5e2967b625c5dc6f
vmt-jvelez <vmt-jvelez@bancoguayaquil.com>
2022-06-10 18:42:27 -0500

    Me toca la clase 34

--- src/app/components/inicio/login/login.component.ts
+++ src/app/components/inicio/login/login.component.ts
@@ -10,35 +10,41 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  
+  loading = false;
+
   login: FormGroup;
 
-  constructor(private fb: FormBuilder, 
-              private toastr: ToastrService,
-              private router: Router ) {     
-    this.login= this.fb.group({
-      usuario:['', Validators.required],
+  constructor(private fb: FormBuilder,
+    private toastr: ToastrService,
+    private router: Router) {
+    this.login = this.fb.group({
+      usuario: ['', Validators.required],
       password: ['', Validators.required]
     });
   }
-  
+
   ngOnInit(): void {
   }
 
-  log(): void{
+  log(): void {
     console.log(this.login);
-    const usuario: Usuario={
-      nombreUsuario : this.login.value.usuario,
-      password : this.login.value.password 
+    const usuario: Usuario = {
+      nombreUsuario: this.login.value.usuario,
+      password: this.login.value.password
     }
+    this.loading=true;
+    setTimeout(() => {
+      if (usuario.nombreUsuario === 'juank' && usuario.password === 'juank123') {
+        this.login.reset();
+        this.router.navigate(['/dashboard']);
+      } else {
+        this.toastr.error("Usuario o Contraseña incorrectos", "Error");
+        this.login.reset();
+      }
+      this.loading=false;
+    }, 3000);
 
-    if (usuario.nombreUsuario==='juank' && usuario.password==='juank123') {
-      this.login.reset();
-      this.router.navigate(['/dashboard']);
-    }else{
-      this.toastr.error("Usuario o Contraseña incorrectos","Error");
-      this.login.reset();
-    }
-  console.log(usuario);
-  }  
+
+    console.log(usuario);
+  }
 }
