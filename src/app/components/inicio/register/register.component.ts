e696f52401cb0a5901318312efd2d1ad89c75dfc
jcvellez <jcvellez@live.com>
2022-07-24 16:15:57 -0500

    Me toca la clase 59 seccion 6

--- src/app/components/inicio/register/register.component.ts
+++ src/app/components/inicio/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
   register: FormGroup;
-  constructor(private fb: FormBuilder) {
+  loading = false;
+  constructor(private fb: FormBuilder,
+              private usuarioService:UsuarioService,
+              private router: Router,
+              private toastr: ToastrService) {
     this.register = this.fb.group({
       usuario: ['', Validators.required],
       password: ['', [Validators.required, Validators.minLength(4)]],
@@ -21,8 +28,25 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  registrarUsuario(): void {
+  registrarUsuario(): void { 
     console.log(this.register);
+    const usuario: Usuario={
+      nombreUsuario: this.register.value.usuario,
+      password: this.register.value.password 
+    }
+    this.loading = true;
+    this.usuarioService.saveUser(usuario).subscribe(data=>{
+      console.log(data);
+      this.toastr.success('El usuario '+usuario.nombreUsuario+' fue registrado con exito','Usuario Registrado!');  
+      this.router.navigate(['/inicio/login']);
+      this.loading= false;
+      
+    }, error =>{
+      this.loading= false;
+      console.log(error);  
+      this.toastr.error(error.error.message,'Usuario ya existe!');    
+      this.register.reset();      
+    });
   }
 
   chekPassword(group: FormGroup): any {   
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Usuario } from "../models/usuario";
 
@@ -14,8 +15,8 @@ export class UsuarioService {
     this.myAppUrl = environment.endpoint;
     this.myApiUrl = '/api/Usuario';
   }
-
-  saveUser(usuario: Usuario):void {
-
+  //http://localhost:21910/api/Usuario -- POST
+  saveUser(usuario: Usuario): Observable<any> {
+    return this.http.post(this.myAppUrl + this.myApiUrl, usuario);
   };
 }
